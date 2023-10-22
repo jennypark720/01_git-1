69c6c9f1d1c6ad1df7b6b0c078ff1c88c06f25b4
jennypark720 <74871853+jennypark720@users.noreply.github.com>
2023-10-22 19:26:44 +0200

    Update generateSierpinski.R

--- R/generateSierpinski.R
+++ R/generateSierpinski.R
@@ -1,5 +1,18 @@
 
 generateSierpinski <- function(rows) {
-  # stub function that generates random rows
-  data.frame(x=runif(rows), y=runif(rows))
+ data <- matrix(0, nrow=rows, ncol=2)
+  
+  vertices <- matrix(c(0.5, 0, 1, 0, sqrt(3)/2, sqrt(3)/2), ncol=2)
+  
+  current_point <- vertices[sample(1:3, 1), ]
+  
+  for (i in 1:rows) {
+    vertex <- vertices[sample(1:3, 1), ]
+    current_point <- (current_point + vertex) / 2
+    data[i, ] <- current_point
+  }
+ 
+  return(as.data.frame(data))
+  
+  data[,2] <- 1 - data[,2]
 }
